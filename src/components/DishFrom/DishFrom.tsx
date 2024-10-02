45dcef2ad97dd493cc10c9dab45082fb8904fa9f
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-03 04:50:00 +0600

    переименовал description на email и добавил проверку на точку в email

--- src/components/DishFrom/DishFrom.tsx
+++ src/components/DishFrom/DishFrom.tsx
@@ -8,11 +8,11 @@ interface Props {
 const DishFrom: React.FC<Props> = ({addNewDish}) => {
   const [newDish, setNewDish] = useState<IDishMutation>({
     name: '',
-    description: '',
+    email: '',
     urlImage: '',
     price: 0,
   });
-  const changeDish = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const changeDish = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewDish(prevState => {
       return {
         ...prevState,
@@ -20,10 +20,16 @@ const DishFrom: React.FC<Props> = ({addNewDish}) => {
       }
     })
   }
+  const validMail = (email: string) => {
+    const mailRegx = /\./;
+    return mailRegx.test(email);
+  }
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (newDish.name.trim().length === 0 && newDish.description.trim().length === 0) {
+    if (newDish.name.trim().length === 0 && newDish.email.trim().length === 0) {
       alert('write text');
+    } else if (!validMail(newDish.email)) {
+      alert('write email');
     } else {
       addNewDish({
         id: new Date().toISOString(),
@@ -32,7 +38,7 @@ const DishFrom: React.FC<Props> = ({addNewDish}) => {
       });
       setNewDish({
         name: '',
-        description: '',
+        email: '',
         urlImage: '',
         price: 0,
       });
@@ -40,9 +46,9 @@ const DishFrom: React.FC<Props> = ({addNewDish}) => {
   }
   return (
     <form onSubmit={onSubmit}>
-      <h3>Add new ...</h3>
+      <h3>Add new user</h3>
       <div className="form-group mb-2">
-        <label htmlFor="name">Title:</label>
+        <label htmlFor="name">Name:</label>
         <input name="name"
                id="name"
                type="text"
@@ -53,14 +59,15 @@ const DishFrom: React.FC<Props> = ({addNewDish}) => {
         ></input>
       </div>
       <div className="form-group mb-2">
-        <label htmlFor="description">Description:</label>
-        <textarea name="description"
-                  id="description"
-                  className="form-control"
-                  required
-                  value={newDish.description}
-                  onChange={changeDish}
-        ></textarea>
+        <label htmlFor="email">Email:</label>
+        <input type="email"
+               name="email"
+               id="email"
+               className="form-control"
+               required
+               value={newDish.email}
+               onChange={changeDish}
+        ></input>
       </div>
       <div className="form-group mb-2">
         <label htmlFor="urlImage">Url image:</label>
