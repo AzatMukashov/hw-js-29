1bed392bb3d9510435c033680786dc8310e7151f
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-03 12:09:38 +0600

    поменял ключ urlImage на роли

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,13 +2,13 @@ export interface IUser {
   id: string;
   name: string;
   email: string;
-  urlImage: string;
+  role: 'user' | 'editor' | 'admin';
   isActive: boolean;
 }
 
 export interface IUserMutation {
   name: string;
   email: string;
-  urlImage: string;
+  role: 'user' | 'editor' | 'admin';
   isActive: boolean;
 }
